35a6a457ec6ac132244af1604769bfaed54d067b
Robert Boloc <robertboloc@gmail.com>
2014-08-31 17:02:11 +0100

    fixed formatting

--- src/Rb/Generator/Zf2ComponentsList.php
+++ src/Rb/Generator/Zf2ComponentsList.php
@@ -12,87 +12,85 @@ class Zf2ComponentsList
      *
      * @var \Zend\Console\Console
      */
-	protected $console;
+    protected $console;
 
     /**
      * Contains the scanned components.
      *
      * @var array
      */
-	protected $components = array();
+    protected $components = array();
 
-	public function scan($project)
-	{
-		//@TODO: allow scan of individual files
-		// First check if target is a directory
-		if(!is_dir($project)) {
-			$this->getConsole()->writeLine($project . ' is not a directory!');
-			return $this;
-		}
+    public function scan($project)
+    {
+        //@TODO: allow scan of individual files
+        // First check if target is a directory
+        if (!is_dir($project)) {
+            $this->getConsole()->writeLine($project . ' is not a directory!');
+            return $this;
+        }
 
-	    $directoryIterator = new \RecursiveDirectoryIterator($project);
-    	$iteratorIterator = new \RecursiveIteratorIterator($directoryIterator);
+        $directoryIterator = new \RecursiveDirectoryIterator($project);
+        $iteratorIterator = new \RecursiveIteratorIterator($directoryIterator);
 
-    	//@TODO: make ignored paths a param
-	    // Exclude the paths containing vendor and non-php files
-    	$regexIterator = new \RegexIterator($iteratorIterator, '/^(?!.*vendor.*).*\.php$/i', \RecursiveRegexIterator::GET_MATCH);
+        //@TODO: make ignored paths a param
+        // Exclude the paths containing vendor and non-php files
+        $regexIterator = new \RegexIterator($iteratorIterator, '/^(?!.*vendor.*).*\.php$/i', \RecursiveRegexIterator::GET_MATCH);
 
-	    foreach($regexIterator as $fileMatches) {
-	        foreach($fileMatches as $fileMatch) {
-	            $fileContent = file_get_contents($fileMatch);
+        foreach ($regexIterator as $fileMatches) {
+            foreach ($fileMatches as $fileMatch) {
+                $fileContent = file_get_contents($fileMatch);
 
-	            preg_match_all('/Zend\\\\[a-zA-Z0-9]*/', (string)$fileContent, $matches, PREG_PATTERN_ORDER);
+                preg_match_all('/Zend\\\\[a-zA-Z0-9]*/', (string) $fileContent, $matches, PREG_PATTERN_ORDER);
 
-	            foreach($matches as $submatches) {
-	                foreach($submatches as $match) {
-	                    $zendComponent = strtolower(str_replace('\\', '-', $match));
-	                    $componentParts = explode('-', $zendComponent);
+                foreach ($matches as $submatches) {
+                    foreach ($submatches as $match) {
+                        $zendComponent = strtolower(str_replace('\\', '-', $match));
+                        $componentParts = explode('-', $zendComponent);
 
-	                    if($componentParts[0] !== '' && $componentParts[1] !== '') {
-	                        $this->components[$zendComponent] = true;
-	                    }
-	                }
-	            }
+                        if ($componentParts[0] !== '' && $componentParts[1] !== '') {
+                            $this->components[$zendComponent] = true;
+                        }
+                    }
+                }
+            }
+        }
 
-	        }
-    	}
+        return $this;
+    }
 
-    	return $this;
-	}
-
-	public function toFile($file)
-	{
+    public function toFile($file)
+    {
         $composer = json_decode(file_get_contents($file), true);
 
         // Remove the zendframework/zendframework
         unset($composer['require']['zendframework/zendframework']);
 
         // Add all the found components
-        foreach(array_keys($this->components) as $component) {
+        foreach (array_keys($this->components) as $component) {
             $composer['require']['zendframework/' . $component] = '2.*';
         }
 
         file_put_contents($file, json_encode($composer, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES));
-	}
+    }
 
-	public function toConsole()
-	{
+    public function toConsole()
+    {
         $this->getConsole()->writeLine(
-            'Replace "zendframework/zendframework" in your composer.json file with :',
-            Color::YELLOW
+                'Replace "zendframework/zendframework" in your composer.json file with :', Color::YELLOW
         );
         $componentsCount = count($this->components);
         $count = 0;
-        foreach(array_keys($this->components) as $component) {
+        foreach (array_keys($this->components) as $component) {
             $suffix = '';
 
             $count++;
-            if($count < $componentsCount) {
+            if ($count < $componentsCount) {
                 $suffix = ',';
             }
             $this->getConsole()->writeLine('"zendframework/' . $component . '": "2.*"' . $suffix);
         }
-	}
+    }
 
     /**
      * Returns the current list of found components.
@@ -111,10 +109,11 @@ class Zf2ComponentsList
      */
     public function getConsole()
     {
-        if(!$this->console) {
+        if (!$this->console) {
             $this->console = Console::getInstance();
         }
 
         return $this->console;
     }
+
 }
